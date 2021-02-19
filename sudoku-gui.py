db002975b8ab05a326c021bf0d1cc80785d059e0
JaxLabs <jacquelyneleanorpowell@gmail.com>
2021-02-18 22:53:06 -0800

    Finished layout of board for sudoku GUI and added win checker
    Finished layout of board for sudoku GUI and added win checker

--- sudoku-gui.py
+++ sudoku-gui.py
@@ -34,7 +34,29 @@ class SudokuGame(object):
 
     def __init__(self, board_map):
         self.board_map = board_map
-        self.build = SudokuBoard(board_map)
+        self.game_layout = SudokuBoard(board_map)
 
     def start(self):
         self.game_over = False
+        self.layout = []
+        for i in xrange(9):
+            self.layout.append([])
+            for j in xrange(9):
+                self.layout[i].append(self.game_layout[i][j])
+
+    def win_check(self):
+        #check row 
+        for row in xrange(9):
+            if not self._check_row(row):
+                return False
+        #check colum
+        for column in xrange(3):
+            if not self._check_colum(colum):
+                return False
+        #check box
+        for row in xrange(3):
+            for coulum in xrange(3):
+                if not self._check_box(row, colum):
+                    return False
+        self.game_over = True
+        return True
