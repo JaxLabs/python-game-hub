8c85e4b8ff4dc8e45212c679c8fc29be2ea3cff0
JaxLabs <jacquelyneleanorpowell@gmail.com>
2021-02-21 15:00:56 -0800

    sudoku UI bug fixes and gid
    fixed alot of bugs. Added logic for grid to show up and set up for solve and clear buttons.

--- sudoku-gui.py
+++ sudoku-gui.py
@@ -1,5 +1,5 @@
 import argparse
-from Tkinter import Tk, Canvas, Frame, Button, BOTH, TOP, BOTTOM
+from tkinter import *
 
 BOARDS = ['easy', 'medium', 'hard', 'error']
 
@@ -38,24 +38,24 @@ class SudokuGame(object):
     def start(self):
         self.game_over = False
         self.layout = []
-        for i in xrange(9):
+        for i in range(9):
             self.layout.append([])
-            for j in xrange(9):
+            for j in range(9):
                 self.layout[i].append(self.game_layout[i][j])
 
     def win_check(self):
         #check row 
-        for row in xrange(9):
+        for row in range(9):
             if not self._check_row(row):
                 return False
         #check colum
-        for column in xrange(3):
-            if not self._check_colum(colum):
+        for column in range(9):
+            if not self._check_colum(column):
                 return False
         #check box
-        for row in xrange(3):
-            for coulum in xrange(3):
-                if not self._check_box(row, colum):
+        for row in range(3):
+            for column in range(3):
+                if not self._check_box(row, column):
                     return False
         self.game_over = True
         return True
@@ -66,21 +66,71 @@ class SudokuGame(object):
     def _check_row(self, row):
         return self.__check_box(self.puzzle[row])
 
-    def _check_column(self, row, column):
+    def _check_column(self, column):
         return self._check_box(
-            [self.layout[row][column] for row in xrange(9)]
+            [self.layout[row][column] for row in range(9)]
         )
 
     def __check_square(self, row, column):
         return self.__check_box(
             [
                 self.layout[r][c]
-                for r in xrange(row * 3, (row + 1) * 3)
-                for c in xrange(column * 3, (column + 1) * 3)
+                for r in range(row * 3, (row + 1) * 3)
+                for c in range(column * 3, (column + 1) * 3)
             ]
         )
 
+class sudokuUI(Frame):
+    #draw board in tkiter
+    def __init__(self, parent, game):
+        self.game = game
+        self.parent = parent
+        Frame.__init__(self, parent)
 
+        self.row, self.col = 0, 0
+
+        self.__initUI()
+    
+    def _initUI(self):
+        self.parent.title("SUDOKU")
+        self.pack(fill=BOTH, expand=1)
+        self.canvas = Canvas(self,
+            width = WIDTH,
+            height = HEIGHT
+        )
+        self.canvas.pack(fill=BOTH, side=TOP)
+        clear_button = Button(self,
+            text = 'Clear', #switch out for icon later
+            command=self.__clear_answers)
+        clear_button.pack(fill=BOTH, side=BOTTOM)
+
+        solve_button = Button(self,
+            text = 'Solve',
+            command = self._solve_puzzle)
+        solve_button.pack(fill=BOTH, side=BOTTOM)
+
+        self.__draw_grid()
+        self.__draw_numbers()
+
+        self.canvas.bind("<Button-1>", self.__cell_clicked)
+        self.canvas.bind("<Key>", self.__key_pressed)
+
+    def _draw_grid(self):
+        #draws sudoku puzzle grid of 9x9 on the canvas 
+        for i in range(10):
+            color = "black" if i % 3 == 0 else "gray"
+
+            x0 = MARGIN + i * SIDE
+            y0 = MARGIN
+            x1 = MARGIN + i * SIDE
+            y1 = HEIGHT - MARGIN
+            self.canvas.create_line(x0, y0, x1, y1, fill=color)
+
+            x0 = MARGIN
+            y0 = MARGIN + i * SIDE
+            x1 = WIDTH - MARGIN
+            y1 = MARGIN + i * SIDE
+            self.canvas.create_line(x0, y0, x1, y1, fill=color)
 
 
 
